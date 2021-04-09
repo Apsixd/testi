4ce90bb2b952480e93f5307655c22c6e650eba71
Japi1984 <82207379+Japi1984@users.noreply.github.com>
2021-04-09 19:45:17 +0300

    jari

--- ot1/test/src/com/company/Main.java
+++ ot1/test/src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("git testi 2");
+        System.out.println("jari testi");
     }
 }
